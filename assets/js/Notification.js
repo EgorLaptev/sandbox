1b2cb9df5838e9aa6a4cfa3541044c3aebd6e83b
EgorLaptev <egor.lapteffff@gmail.com>
2021-07-17 19:02:48 +0300

    Review Notification's code

--- assets/js/Notification.js
+++ assets/js/Notification.js
@@ -12,7 +12,7 @@ export default class Notification {
         },
         color: '#222',
         class: 'game-notification',
-        time: 10 // In seconds
+        time: 5 // In seconds
     }
 
     constructor(content) {
@@ -20,27 +20,52 @@ export default class Notification {
         const notification = document.createElement('div');
         notification.classList.add(Notification.config.class);
 
+        /* Notification content */
         notification.innerHTML = `${content}`;
 
+        /* Notification style */
         notification.style =
             `
                 position: fixed;
                 top: ${ 5 + Notification.list.length * 55 }px;
                 right: 5px;
                 padding: 15px;
+                transition: 1s;
+                opacity: 0;
                 background: ${Notification.config.color};
                 font-size: ${Notification.config.font.size};
                 font-family: ${Notification.config.font.family};
                 color: ${Notification.config.font.color};
             `;
 
+        /* Adding notification */
         document.body.appendChild(notification);
-
         Notification.list.push(notification);
 
+        /* Fade in effect */
+        setTimeout( () => notification.style.opacity = '1', 0);
+
+        /* Close notification */
         setTimeout( () => {
-            Notification.list.shift().remove();
+
+            /* Fade out effect */
+            notification.style.opacity = '0';
+
+            setTimeout( this.close, 1000);
+
         }, Notification.config.time * 1000);
+
+    }
+
+    close() {
+
+
+        /* Delete notification */
+        Notification.list.shift().remove();
+
+        /* Moving other notifications up */
+        for ( let i=0;i<Notification.list.length;i++)
+            Notification.list[i].style.top = `${ 5 + i * 55 }px`;
 
     }
 
